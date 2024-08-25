16ee92133930cbf15dd4d8de2e614d036e038b89
SyncGit12 <144155299+SyncGit12@users.noreply.github.com>
2024-08-25 19:43:16 -0300

    assets
    yeah

---
+++ setup-Windows-allLastest.bat
@@ -0,0 +1,30 @@
+@echo off
+title auto-set-id47
+echo Installing dependencies for building...
+echo This will install 5 GB of stuff but trust me its going to help
+curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
+vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
+echo You can delete the setup file later.
+curl -L -# -O https://github.com/HaxeFoundation/haxe/releases/download/4.3.6/haxe-4.3.6-win64.exe
+haxe-4.3.6-win64.exe
+echo You can delete the setup file later.
+haxelib setup C:\haxelib
+echo haxelib path is C:\haxelib
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib update flixel
+haxelib install newgrounds
+haxelib install SScript
+haxelib install hxCodec
+haxelib install tjson
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+echo Done!
+pause
+exit
---
+++ title state bullshit.hx
@@ -0,0 +1,122 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.system.FlxAssets;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import lime.ui.Window;
+import openfl.Assets;
+import openfl.display.Sprite;
+import openfl.events.AsyncErrorEvent;
+import openfl.events.AsyncErrorEvent;
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+import openfl.events.NetStatusEvent;
+import openfl.media.Video;
+import openfl.net.NetConnection;
+import openfl.net.NetStream;
+import shaderslmfao.BuildingShaders.BuildingShader;
+import shaderslmfao.BuildingShaders;
+import shaderslmfao.ColorSwap;
+import ui.PreferencesMenu;
+
+using StringTools;
+
+#if discord_rpc
+import Discord.DiscordClient;
+#end
+#if desktop
+import sys.FileSystem;
+import sys.io.File;
+import sys.thread.Thread;
+#end
+
+class TitleState extends FlxTransitionableState
+{
+	static var initialized:Bool = false;
+
+	override public function create():Void
+	{
+		super.create();
+
+		if (!initialized)
+		{
+			var diamond:FlxGraphic = FlxGraphic.fromClass(GraphicTransTileDiamond);
+			diamond.persist = true;
+			diamond.destroyOnNoUse = false;
+
+			FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 2, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
+				new FlxRect(0, 0, FlxG.width, FlxG.height));
+			FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 1.3, new FlxPoint(0, 1),
+				{asset: diamond, width: 32, height: 32}, new FlxRect(0, 0, FlxG.width, FlxG.height));
+
+			initialized = true;
+
+			FlxTransitionableState.defaultTransIn.tileData = {asset: diamond, width: 32, height: 32};
+			FlxTransitionableState.defaultTransOut.tileData = {asset: diamond, width: 32, height: 32};
+
+			transIn = FlxTransitionableState.defaultTransIn;
+			transOut = FlxTransitionableState.defaultTransOut;
+		}
+
+		persistentUpdate = true;
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.stageback__png);
+		bg.antialiasing = true;
+		bg.setGraphicSize(Std.int(bg.width * 0.6));
+		bg.updateHitbox();
+		add(bg);
+
+		var logoBl:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.logo__png);
+		logoBl.screenCenter();
+		logoBl.color = FlxColor.BLACK;
+		add(logoBl);
+
+		var logo:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.logo__png);
+		logo.screenCenter();
+		logo.antialiasing = true;
+		add(logo);
+
+		FlxTween.tween(logoBl, {y: logoBl.y + 50}, 0.6, {ease: FlxEase.quadInOut, type: PINGPONG});
+		FlxTween.tween(logo, {y: logoBl.y + 50}, 0.6, {ease: FlxEase.quadInOut, type: PINGPONG, startDelay: 0.1});
+
+		FlxG.sound.playMusic('assets/music/title.mp3', 0, false);
+
+		FlxG.sound.music.fadeIn(4, 0, 0.7);
+	}
+
+	var transitioning:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.keys.justPressed.ENTER && !transitioning)
+		{
+			FlxG.camera.flash(FlxColor.WHITE, 1);
+
+			transitioning = true;
+			FlxG.sound.music.stop();
+
+			new FlxTimer().start(2, function(tmr:FlxTimer)
+			{
+				FlxG.switchState(new PlayState());
+			});
+			FlxG.sound.play('assets/music/titleShoot.mp3', 0.7);
+		}
+
+		super.update(elapsed);
+	}
+}
